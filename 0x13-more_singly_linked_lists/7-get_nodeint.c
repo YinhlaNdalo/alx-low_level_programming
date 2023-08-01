cdb83dbb22223718ef4d36761023ba7676532db1
YinhlaNdalo <nkatyndobe@gmail.com>
2023-08-01 10:41:14 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - func to returns the node at a certain index in a linked list
+ * get_nodeint_at_index - func to returns the node at a certain index
  * @head: the first node in the linked list
  * @index: index of the node to be return
  *
