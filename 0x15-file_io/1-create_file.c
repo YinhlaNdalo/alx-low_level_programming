704df13d8e22fe57fc6e372adefb76d15cb487cb
YinhlaNdalo <nkatyndobe@gmail.com>
2023-08-07 10:44:25 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,10 +2,10 @@
 
 /**
  * create_file - function that creates a file.
- * @filename: name of the file to create.
+ * @filename: file to create.
  * @text_content: string to write to the file.
  *
- * Return: -1 null
+ * Return: -1
  */
 int create_file(const char *filename, char *text_content)
 {
